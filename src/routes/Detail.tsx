6dbaec595d894d2ff19d47cc28ec5744fed52f16
okdohyuk <okdohyuk2@gmail.com>
2021-01-14 19:48:45 +0900

    first commit

---
+++ src/routes/Detail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Detail() {
+  return <div>22</div>;
+}
+
+export default Detail;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainWrap = styled.div`
+  min-height: 100vh;
+  background-color: yellow;
+`;
+
+function Home() {
+  return <MainWrap>home</MainWrap>;
+}
+
+export default Home;
