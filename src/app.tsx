56b55c36bef056d00af10fe164dc9024b630456b
okdohyuk <okdohyuk2@gmail.com>
2021-05-27 11:14:41 +0900

    Use react.lazy

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { Navigation } from 'components';
-import { Home, About, Projects, Contents, NotFound } from 'pages';
+
+const Home = lazy(() => import('pages/Home'));
+const About = lazy(() => import('pages/About'));
+const Projects = lazy(() => import('pages/Projects'));
+const Contents = lazy(() => import('pages/Contents'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navigation />
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/projects" component={Projects}></Route>
-        <Route path="/about" component={About}></Route>
-        <Route path="/contents/:id" component={Contents}></Route>
-        <Route component={NotFound}></Route>
-      </Switch>
-    </BrowserRouter>
+      <Suspense fallback={''}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/about" component={About} />
+          <Route path="/contents/:id" component={Contents} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </>
   );
 }
 
